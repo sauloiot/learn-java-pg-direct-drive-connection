680fcbb2ddba6908301d4e760b8e3de74299be21
sauloiot <saulo.11@hotmail.com>
2021-04-15 22:59:36 -0300

    Files

---
+++ src/main/java/app/Program.java
@@ -0,0 +1,24 @@
+package app;
+
+import db.DB;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Program {
+
+	public static void main(String[] args) throws SQLException {
+		
+		Connection conn = DB.getConnection();
+	
+		Statement st = conn.createStatement();
+			
+		ResultSet rs = st.executeQuery("select * from tb_product");
+			
+		while (rs.next()) {
+			System.out.println(rs.getLong("Id") + ", " + rs.getString("Name"));
+		}
+	}
+}
